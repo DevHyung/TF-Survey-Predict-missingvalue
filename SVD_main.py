f81d797f0ebb49469bc36cda1b9fd870231c4329
DevHyung <khuphj@gmail.com>
2018-01-29 18:19:14 +0900

    :date: 20180129

--- SVD_main.py
+++ SVD_main.py
@@ -6,20 +6,27 @@ date            :2018.01.09
 version         :1.0.0
 python_version  :3.6
 required module :tensorflow, numpy
-develope env    : Mac OS X High Sierra, intel i5 (2Ghz), using cpu
+develope env    :Mac OS X High Sierra, intel i5 (2Ghz), using cpu
 [ref]
 1. https://www.bonaccorso.eu/2017/08/02/svd-recommendations-using-tensorflow/
-2.
+
 """
 import numpy as np
+from numpy import genfromtxt
+#118명 254개
+data = genfromtxt('data.csv', delimiter=',',dtype=np.float32)
+uim = np.nan_to_num(data)
+
 import tensorflow as tf
 import time
 # Set random seed for reproducibility
 np.random.seed(1000)
 #####_____ Set traning variable _____#####
-nb_users = 5000
-nb_products = 2000
-nb_factors = 500
+#nb_users = 5000
+nb_users = 118
+#nb_products = 2000
+nb_products = 254
+nb_factors = 254
 max_rating = 5
 nb_rated_products = 500
 top_k_products = 10
@@ -28,11 +35,11 @@ if __name__ == "__main__":
     # Calculate runtime
     start_time = time.time()
     # Create a random User-Item matrix
-    uim = np.zeros((nb_users, nb_products), dtype=np.float32)
-    for i in range(nb_users):
-        nbp = np.random.randint(0, nb_products, size=nb_rated_products)
-        for j in nbp:
-            uim[i, j] = np.random.randint(1, max_rating + 1)
+    #uim = np.zeros((nb_users, nb_products), dtype=np.float32)
+    #for i in range(nb_users):
+    #    nbp = np.random.randint(0, nb_products, size=nb_rated_products)
+    #    for j in nbp:
+    #        uim[i, j] = np.random.randint(1, max_rating + 1)
     # Create a Tensorflow graph
     graph = tf.Graph()
     with graph.as_default():
@@ -41,9 +48,9 @@ if __name__ == "__main__":
         # SVD
         St, Ut, Vt = tf.svd(user_item_matrix)
         # Compute reduced matrices
-        Sk = tf.diag(St)[0:nb_factors, 0:nb_factors]
+        Sk = tf.diag(St)[0:nb_users, 0:nb_factors]
         Uk = Ut[:, 0:nb_factors]
-        Vk = Vt[0:nb_factors, :]
+        Vk = Vt[0:nb_users, :]
         # Compute Su and Si
         Su = tf.matmul(Uk, tf.sqrt(Sk))
         Si = tf.matmul(tf.sqrt(Sk), Vk)
@@ -59,8 +66,8 @@ if __name__ == "__main__":
     }
     best_items = session.run([best_items_t], feed_dict=feed_dict)
     # Suggestions for user 1000, 1010
-    for i in range(1000, 1010):
-        print('User {}: {}'.format(i, best_items[0][i]))
+    for i in range(1, 10):
+        print('User {}: {}'.format(i, uim[i][best_items[0][i])])
     print("--- %s seconds ---" % (time.time() - start_time))
     """
     my develope SPEC take time show below 
